516175a69ff9033e2706091c1b0d65c23bde2970
Arpita2106 <arpitasagar2106@gmail.com>
2022-07-06 16:20:59 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs  = require('ejs');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req,res){
+  res.render("home");
+});
+
+app.get("/contact", function(req,res){
+  res.render("contact");
+});
+
+app.listen(3000, function(){
+  console.log("Server started at port 3000");
+});
